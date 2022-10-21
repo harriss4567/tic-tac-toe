22e9760f5e5975195e8d6c39b4b18320aaddf1b9
Harris <harris@Harriss-MacBook-Air.local>
2022-10-21 00:58:28 -0400

    Add declarations and win conditions in index.js file

---
+++ index.js
@@ -0,0 +1,23 @@
+// Declarations
+const cells = document.querySelectorAll('.cell');
+const currentStatus = document.querySelector('#currentStatus');
+const playAgain = document.querySelector('#playAgain');
+
+// Win Conditions
+const winConditions = [
+    [0, 1, 2], 
+    [3, 4, 5], 
+    [6, 7, 8], 
+    [0, 3, 6], 
+    [1, 4, 7], 
+    [2, 5, 8], 
+    [0, 4, 8], 
+    [2, 4, 6]
+];
+// Temp placeholders for each cell
+let placeholder = [[''], [''], [''], [''], [''], [''], [''], [''], ['']];
+
+// Current Player and Game Status
+let currentPlayer = 'X';
+let isRunning = false;
+
